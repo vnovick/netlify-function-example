b73c0ee8932edc11e521e32f6b1b3a6c8b4fde15
vnovick <vnovick@gmail.com>
2019-04-18 19:56:09 +0300

    sample hello thing

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,8 @@
 exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
+  const request = JSON.parse(event.body)
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`
+    body: JSON.stringify(request.event.data)
   }
 }
